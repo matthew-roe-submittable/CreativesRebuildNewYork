464b2884a4cdfe81fe40c2432bd6386e7f6a455b
jesse finneman <jesse.finneman@submittable.com>
2022-03-03 15:18:07 -0700

    small update in loop - move up ref form get call

--- config.py
+++ config.py
@@ -24,10 +24,10 @@ project_1_artist_last_name  = "edfb62ed-9f31-49d6-bcf6-dce8ec0589b6"
 project_1_artist_zipcode    = "254429d2-640b-4c8b-9839-73c3e5ba12a7"
 project_1_artist_dob        = "a3c4efb3-c45b-4efc-bee5-6b5c94ed2592"
 
-# Collaboratives Artist Reference 
+# Collaboratives Artist Reference
 reference_form_id           = "a5a47e3e-1aae-4eee-808f-d580aa38fb21"
 
-# Initial Field ID of the reference form - can have up to 9 additional artists 
+# Initial Field ID of the reference form - can have up to 9 additional artists
 reference_form_field_id_1    = "2f5df106-e782-422b-9c39-a3345788b715"
 reference_form_field_id_2    = "da35f4fc-3219-4731-91ed-3e5a46ddc4ef"
 reference_form_field_id_3    = "605e57c4-5488-4a75-a7ed-ad6399ee2085"
@@ -53,7 +53,7 @@ project_2_zipcode_field_id   = "d533967d-d72b-4266-a502-577b6ae6db81"
 internal_form_id            = "f2911a5d-1293-4f84-85b2-63ec0a1e6282"
 # Primary Artist
 internal_form_field_id_1    = "77667d07-9c56-461b-9e80-243b806a4882"
-# Collaborators Artist 
+# Collaborators Artist
 internal_form_field_id_2    = "1a97b9d8-a8ef-4270-b378-5c82a6d7b75c"
 internal_form_field_id_3    = "f4d45c72-5f00-4358-bd8e-02d19710c442"
 internal_form_field_id_4    = "bc6a7400-b138-499c-b6f8-fb4b5082e08c"
@@ -82,13 +82,10 @@ project_1_formId            = "f2ee04a8-8238-401d-863f-8783305b9897"
 project_1_artist_last_name  = "9097486a-79ad-4b50-935d-8008f12b6cf1"
 project_1_artist_dob        = "7cb13021-332e-4278-a98b-4a69ca96ec45"
 project_1_artist_zipcode    = "2d27064a-a57a-4f2e-847e-dee5f5876a43"
-
 # Reference form to hold up to 9 UID per submission
 artist_collab_reference_form = "ad9a40c6-4152-4d21-a16c-9a885b1f2a41"
-
 # Form field id of the reference form in the internal form
 org_reference_form_field_id = "6d3bf23a-df28-4eca-b1fe-ff8504cd6a4a"
-
 # collaborative artist 2-10
 reference_form_field_id_1   = "7a96a260-af5f-4c43-b28c-b53ecf0f0080"
 reference_form_field_id_2   = "b2aa5c71-e99a-4067-a3ea-2d70b442a286"
@@ -99,22 +96,18 @@ reference_form_field_id_6   = "eb5ed7be-9954-400a-b891-4ad48fa98fc3"
 reference_form_field_id_7   = "97ccc2c5-050e-4959-b2e5-add05ed27366"
 reference_form_field_id_8   = "438071ae-e69d-4604-adc6-669d221fe844"
 reference_form_field_id_9   = "bb7d5454-d7c5-4e40-9699-900d4eca770c"
-
 # ref form fields
 reference_form_name_id      = "0182623d-9114-4304-95ed-b2f9abd0ee6b"
 reference_form_dob_id       = "a6c73923-316c-4e50-8b5a-379bc9225111"
 reference_form_zipcode_id   = "a0936d07-ecc6-4031-ba5a-fc7da24f0526"
-
 # Project: GI demo project
 project_id_2                 = "cc425384-5ee6-4063-8642-3155383a6bd1"
 project_2_formId             = "167be692-282c-46c4-baa7-0e8645fdfd89"
 project_2_name_field_id      = "83f4d296-5aaf-428d-be76-905b240be153"
 project_2_dob_field_id       = "3af16b68-d0a8-4dff-9bdb-dbb6bcae5b70"
 project_2_zipcode_field_id   = "b18620bf-ba42-4293-879a-e27286a376df"
-
 # internal form to hold up to 10 UID per submission
 internal_form_id             = "d051aa12-614b-4f30-a9eb-db9f934a317a"
-
 # 10 UID fields
 internal_form_field_id_1     = "b8f1e240-3b57-4c05-8acb-566c79ac5d95"
 internal_form_field_id_2     = "3357efb7-9412-42f0-9109-ddb3b1decff7"
@@ -126,21 +119,17 @@ internal_form_field_id_7     = "59a845ff-83ab-4ffd-a204-d8247335bb73"
 internal_form_field_id_8     = "bf13719d-85da-4acc-83dd-fd16cb196ee5"
 internal_form_field_id_9     = "05bc0950-98c1-4822-bcd8-dc35e3fd4ca9"
 internal_form_field_id_10    = "f7f704be-5ea5-4a68-8f0b-9e4cb5bdd0ab"
-
 # label "Awarded Duplicate"
 label_id_1 = "317972"
-
 # label "Not Awarded Duplicate"
 label_id_2 = "317973"
-
 # label "Pending Duplicate"
 label_id_3 = "317972"
-
+'''
 # Data structure to replace Database
 # volatile memory used and thrown away
 # list of dicts
 uid_data_struct = []
-'''
 
 # dict structure
 """
